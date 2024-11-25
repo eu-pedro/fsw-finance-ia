63b89674be746884005d70a85b07c026009a45c8
pedro henrique <pedro.henrike2812@gmail.com>
2024-11-25 17:48:21 -0300

    chore: add git hook setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-
-  const { userId } = await auth()
-  if (!userId) redirect("/login")
+  const { userId } = await auth();
+  if (!userId) redirect("/login");
 
   return (
     <div>
