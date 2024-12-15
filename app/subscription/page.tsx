dbad7202b6dab95f6c41872d56f886d5e2180949
Pedro Henrique <pedro.henrike2812@gmail.com>
2024-12-15 19:40:18 -0300

    feat: ensure transactions and subscripstions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,10 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import { Navbar } from "../_components/navbar";
 
-export default function SubscriptionPage() {
+export default async function SubscriptionPage() {
+  const { userId } = await auth();
+
+  if (!userId) redirect("/login");
   return <Navbar />;
 }
