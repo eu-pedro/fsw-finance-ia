a84a7b0980e412d929cbc50a56f7aa50f61ccbca
Pedro Henrique <pedro.henrike2812@gmail.com>
2024-12-31 22:26:12 -0300

    fix: ensure responsive login screen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,19 +6,25 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function LoginPage() {
+  const { userId } = await auth();
 
-  const { userId } = await auth()
-
-  if (userId) redirect("/")
+  if (userId) redirect("/");
 
   return (
-    <div className="grid grid-cols-2 h-full">
-      <div className="flex h-full flex-col justify-center p-8 max-w-[550px] mx-auto">
-        <Image src="/logo.svg" width={173} height={39} alt="Finance IA" className="mb-8" />
-        <h1 className="text-4xl font-bold mb-3">Bem vindo</h1>
-        <p className="text-muted-foreground mb-8">
-          A Finance
-          AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.
+    <div className="grid h-full grid-cols-1 lg:grid-cols-2">
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance IA"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
         </p>
         <SignInButton>
           <Button variant="outline">
@@ -27,10 +33,14 @@ export default async function LoginPage() {
           </Button>
         </SignInButton>
       </div>
-      <div className="relative h-full w-full">
-        <Image src="/login.png" alt="Faça login" fill className="object-cover" />
-
+      <div className="relative hidden h-full w-full lg:block">
+        <Image
+          src="/login.png"
+          alt="Faça login"
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
-  )
+  );
 }
